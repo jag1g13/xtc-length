53b99545883f12f433e268469625ff017d2c0ee1
James Graham <J.A.Graham@soton.ac.uk>
2015-09-16 11:38:03 +0100

    Removed unused code and made rounding simpler

--- xtc-length.c
+++ xtc-length.c
@@ -1,13 +1,45 @@
 #include <stdio.h>
 #include <inttypes.h>
-#include <string.h>
 
-int get_xtc_num_frames(const char *filename, int *nframes, int *natoms);
-uint32_t u4_from_buffer(const uint8_t *b);
-float float_from_buffer(const uint8_t *b);
-long file_size(FILE *f);
-void print_header(const uint8_t *header);
-int round_up(int numToRound, int multiple);
+/*
+ * Program for finding the number of frames and atoms in a GROMACS XTC file.
+ * Inspired by a post on the gmx-developers mailing list in 2012:
+ * https://mailman-1.sys.kth.se/pipermail/gromacs.org_gmx-developers/2012-June/005937.html
+ */
+
+uint32_t u4_from_buffer(const uint8_t *b){
+    return b[0] << 24 | b[1] << 16 | b[2] << 8 | b[3] << 0;
+}
+
+int get_xtc_num_frames(const char *filename, int *nframes, int *natoms){
+    // Open XTC file as binary
+    FILE *xtc = fopen(filename, "rb");
+    if(!xtc) return -1;
+
+    // Read header of first frame
+    uint8_t header[92];
+    if(!fread(header, 92, 1, xtc)){
+        fclose(xtc);
+        return -1;
+    }
+    *natoms = u4_from_buffer(header+4);
+    uint32_t frame_size = u4_from_buffer(header+88);
+
+    *nframes = 1;
+    while(1){
+        // Round frame_size up to nearest 4
+        uint32_t skip = (frame_size+3) & ~((uint32_t)4);
+        // Skip over frame to next header
+        fseek(xtc, skip, SEEK_CUR);
+        // Read in next frame size
+        if(!fread(header, 92, 1, xtc)) break;
+        frame_size = u4_from_buffer(header+88);
+        (*nframes)++;
+    }
+
+    fclose(xtc);
+    return 0;
+}
 
 int main(const int argc, const char *argv[]){
     if(argc < 2){
@@ -23,58 +55,4 @@ int main(const int argc, const char *argv[]){
 
     printf("Trajectory contains %d frames of %d atoms.\n", nframes, natoms);
     return 0;
-}
-
-int get_xtc_num_frames(const char *filename, int *nframes, int *natoms){
-    FILE *xtc = fopen(filename, "rb");
-    if(!xtc) return -1;
-
-    uint8_t header[92];
-    if(!fread(header, 92, 1, xtc)) return -1;
-    uint32_t magic = u4_from_buffer(header);
-    *natoms = u4_from_buffer(header+4);
-    uint32_t frame_size = u4_from_buffer(header+88);
-
-    *nframes = 1;
-    while(1){
-        int skip = round_up(frame_size, 4);
-        fseek(xtc, skip, SEEK_CUR);
-        if(!fread(header, 92, 1, xtc)) break;
-//        print_header(header);
-        frame_size = u4_from_buffer(header+88);
-        (*nframes)++;
-    }
-
-    fclose(xtc);
-    return 0;
-}
-
-void print_header(const uint8_t *header){
-    printf("%4d ", u4_from_buffer(header));
-    printf("%4d ", u4_from_buffer(header + 4));
-    printf("%8d ", u4_from_buffer(header + 8));
-    printf("%6.3f ", float_from_buffer(header + 16));
-    printf("%6.3f ", float_from_buffer(header + 32));
-    printf("%6.3f ", float_from_buffer(header + 48));
-    printf("%6d ", u4_from_buffer(header + 88));
-    printf("\n");
-}
-
-uint32_t u4_from_buffer(const uint8_t *b){
-    return b[0] << 24 | b[1] << 16 | b[2] << 8 | b[3] << 0;
-}
-
-float float_from_buffer(const uint8_t *b){
-    uint32_t tmp = u4_from_buffer(b);
-    float result;
-    memcpy(&result, &tmp, sizeof(tmp));
-    return result;
-}
-
-int round_up(const int numToRound, const int multiple){
-    if(multiple == 0) return numToRound;
-
-    int remainder = numToRound % multiple;
-    if(remainder == 0) return numToRound;
-    return numToRound + multiple - remainder;
 }
